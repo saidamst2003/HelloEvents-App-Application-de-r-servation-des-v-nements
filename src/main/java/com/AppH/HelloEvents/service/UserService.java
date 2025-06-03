766a286be01b7b2d6e3fd84619f0ed97b1ed2e92
saidamst2003 <saida2003mst@gmail.com>
2025-06-03 17:03:23 +0100

    j'ai creerles les methode dans user service

--- src/main/java/com/AppH/HelloEvents/service/UserService.java
+++ src/main/java/com/AppH/HelloEvents/service/UserService.java
@@ -1,8 +1,13 @@
 package com.AppH.HelloEvents.service;
 
+import com.AppH.HelloEvents.model.User;
 import com.AppH.HelloEvents.repository.UserReposetory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 
@@ -11,6 +16,20 @@ public class UserService {
 @Autowired
     private UserReposetory userRepository;
 
+
+
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
+
+    public Page<User> findByRole(String role, Pageable pageable) {
+        return userRepository.findByRole(role, pageable);
+    }
     public int countUsersByRole(String role) {
         return userRepository.countUserByrole(role);
     }
